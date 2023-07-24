b3818356fe841c3cdd4f93fdb5766febde3c1a58
Dionne <mylife33@hotmail.com>
2023-07-24 16:08:23 -0500

    added copy for about and landing pages

--- src/components/MarketingPages/About.js
+++ src/components/MarketingPages/About.js
@@ -2,8 +2,85 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="main-page">
-      <h3>About</h3>
+    <div div className="main-page">
+      <h2>
+        About Rabbi Rabbit: Igniting a Passion for Hebrew Language Mastery
+      </h2>
+      <p>
+        At Rabbi Rabbit, our journey began with a burning passion for Hebrew
+        language and an unwavering desire to make learning Hebrew vocabulary an
+        immersive and effective experience. Inspired by the challenges faced
+        during our own study of Hebrew, we set out to create a web app that
+        would revolutionize the way language learners approached vocabulary
+        practice.
+      </p>
+      <h3>The Genesis of Rabbi Rabbit: A Personal Quest</h3>
+      <p>
+        As language enthusiasts and avid students of Hebrew, we discovered that
+        traditional language learning methods, such as flashcards and repetitive
+        drills, lacked the engagement and practicality we craved. We yearned for
+        a dynamic and interactive platform that mirrored real-life language
+        usage, allowing us to type in responses and encounter words in
+        meaningful contexts.
+      </p>
+      <h3>The Vision Unveiled: A New Path to Proficiency</h3>
+      <p>
+        Driven by our dedication to promoting authentic language learning, we
+        embarked on a mission to develop a web app that combined cutting-edge
+        technology with the proven effectiveness of spaced repetition. Rabbi
+        Rabbit was born - a platform that would harness the power of typing
+        exercises and spaced repetition to ignite a passion for Hebrew language
+        mastery like never before.
+      </p>
+      <h3>The Rabbi Rabbit Difference: Interactive Learning Redefined</h3>
+      <p>
+        Rabbi Rabbit stands apart by offering an engaging and user-friendly
+        learning experience. Say farewell to monotonous memorization and embrace
+        the thrill of interactive typing exercises that simulate real
+        conversations. Our platform adapts to your progress, ensuring you
+        encounter vocabulary at precisely timed intervals for optimal retention.
+      </p>
+      <h3>Our Commitment to Your Success: A Growing Word Library</h3>
+      <p>
+        With the aim to cater to learners of all levels, Rabbi Rabbit's word
+        library is continuously expanding. From fundamental Hebrew words for
+        beginners to sophisticated expressions for advanced learners, we strive
+        to provide a comprehensive and ever-evolving resource for all language
+        enthusiasts.
+      </p>
+      <h3>Your Learning Journey, Your Pace: Flexibility at Its Finest</h3>
+      <p>
+        At Rabbi Rabbit, we celebrate individuality and respect that every
+        learner has their own pace. Whether you're a dedicated language student
+        or squeezing in study time amidst a busy schedule, our platform empowers
+        you to tailor your learning journey according to your preferences.
+      </p>
+      <h3>Embark on a Journey of Excellence: The Future Awaits</h3>
+      <p>
+        We are thrilled to share Rabbi Rabbit with the world - a dream come true
+        for Hebrew language learners seeking an extraordinary path to
+        proficiency. Our passion lies in unlocking the potential of learners
+        like you, guiding you on a transformative journey to fluency and
+        cultural connection.
+      </p>
+      <h3>Join the Rabbi Rabbit Community: Ignite Your Hebrew Adventure!</h3>
+      <p>
+        We welcome you to embrace the richness of the Hebrew language with Rabbi
+        Rabbit. Unlock the true essence of Hebrew vocabulary through our
+        innovative web app, and embark on an adventure that will open doors to
+        endless possibilities in culture, communication, and personal growth.
+      </p>
+      <h3>Let's Conquer Hebrew Together: Join Rabbi Rabbit Today!</h3>
+      <p>
+        As we continue to evolve and expand, we invite you to be a part of the
+        Rabbi Rabbit community. Together, we will revolutionize language
+        learning and inspire a new generation of Hebrew language enthusiasts.
+      </p>
+      <p>
+        Start your journey with Rabbi Rabbit now, and unleash the language
+        enthusiast within you. Welcome to a world of Hebrew vocabulary mastery
+        like never before!
+      </p>
     </div>
   );
 }
--- src/components/MarketingPages/LandingPage.js
+++ src/components/MarketingPages/LandingPage.js
@@ -3,8 +3,97 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div className="main-page">
-      <h3>Welcome to Rabbi Rabbit!</h3>
-      <p> This is the main landing page. </p>
+      <h1>Welcome to Rabbi Rabbit!</h1>
+      <h2>Master Hebrew Vocabulary: Your Gateway to Language Proficiency</h2>
+      <p>
+        Welcome to the ultimate destination for learning and practicing Hebrew
+        vocabulary - a dynamic platform designed to propel you towards language
+        mastery through interactive typing exercises and cutting-edge spaced
+        repetition technology!
+      </p>
+
+      <h2>Why Choose Our Hebrew Vocabulary Platform?</h2>
+      <ol>
+        <li>
+          <strong>Interactive Learning Experience:</strong> Say goodbye to
+          tedious rote memorization! Our platform engages you with interactive
+          typing exercises that make learning Hebrew vocabulary fun and
+          effective. The more you type, the more you learn!
+        </li>
+        <li>
+          <strong>Spaced Repetition for Lasting Results:</strong> Unlock the
+          power of spaced repetition - a scientifically proven technique that
+          optimizes memory retention. Our intelligent algorithm adapts to your
+          progress, ensuring you review words at the perfect intervals for
+          long-term recall.
+        </li>
+        <li>
+          <strong>Extensive Word Library:</strong> Dive into an ever-expanding
+          database of Hebrew words curated by language experts. From basic
+          everyday terms to advanced expressions, we've got you covered across
+          all proficiency levels.
+        </li>
+        <li>
+          <strong>Learn at Your Own Pace:</strong> No rush, no pressure. Our
+          platform lets you customize your learning journey. Study as
+          intensively or casually as you prefer, and watch your vocabulary
+          flourish over time.
+        </li>
+        <li>
+          <strong>Real-world Context:</strong> Contextual learning is the key to
+          language proficiency. Our vocabulary exercises are contextualized,
+          enabling you to understand how words are used in various scenarios,
+          conversations, and sentences.
+        </li>
+        <li>
+          <strong>Mobile and Desktop Friendly:</strong> Learn anytime, anywhere!
+          Access our platform seamlessly from your computer, tablet, or
+          smartphone. Study on your daily commute, during lunch breaks, or from
+          the comfort of your home.
+        </li>
+        <li>
+          <strong>Track Your Progress:</strong> Stay motivated by tracking your
+          progress and witnessing your improvement. Celebrate milestones as you
+          master more and more Hebrew words, building your confidence along the
+          way.
+        </li>
+      </ol>
+
+      <h2>What Our Users Say:</h2>
+      <blockquote>
+        <p>
+          "I've tried several Hebrew language platforms, but this one stands
+          out! The spaced repetition really makes a difference, and the typing
+          exercises are both challenging and enjoyable." - Rachel W.
+        </p>
+        <p>
+          "Learning Hebrew vocabulary has never been this engaging. The platform
+          keeps me coming back for more, and my language skills have improved
+          significantly in just a few weeks!" - Ethan K.
+        </p>
+      </blockquote>
+
+      <h2>Join the Hebrew Vocabulary Revolution:</h2>
+      <p>
+        Are you ready to embark on an exciting journey towards Hebrew language
+        proficiency? Empower yourself with a powerful tool that utilizes spaced
+        repetition and interactive typing exercises to accelerate your
+        vocabulary learning.
+      </p>
+      <p>
+        <strong>
+          Sign up now for our limited-time trial offer and take your first step
+          towards Hebrew fluency!
+        </strong>{" "}
+        Harness the potential of our platform to unlock new opportunities in
+        cultural exchange, travel, business, and personal growth.
+      </p>
+
+      <p>
+        Begin your transformation today and embrace the richness of the Hebrew
+        language! The world of Hebrew vocabulary awaits you - start your
+        adventure now!
+      </p>
     </div>
   );
 }
